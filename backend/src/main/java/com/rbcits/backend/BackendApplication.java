bdba9a281525bcfe655634880a2b990fc489ea08
dandangerero21 <roldanmhergerero@gmail.com>
2026-04-21 21:56:42 +0800

    added timezone to backend

--- backend/src/main/java/com/rbcits/backend/BackendApplication.java
+++ backend/src/main/java/com/rbcits/backend/BackendApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.mail.autoconfigure.MailSenderAutoConfiguration;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = { MailSenderAutoConfiguration.class })
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Manila"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
